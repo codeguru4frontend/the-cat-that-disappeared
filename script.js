008c2c2a4e4bdd5b7f33a666f24226ce6dbb827f
codeguru4frontend <codeguru4frontend@gmail.com>
2023-12-08 10:53:00 +0100

    added the-cat-that-disappeared

---
+++ script.js
@@ -0,0 +1,19 @@
+/* First, add the code here */
+
+// First, add the code here
+let catImage = document.querySelector('img');
+
+setTimeout(() => {
+  // Then, move the code here instead.
+  catImage.classList.add('hidden');
+}, 3000);
+
+
+
+
+
+
+
+
+
+
